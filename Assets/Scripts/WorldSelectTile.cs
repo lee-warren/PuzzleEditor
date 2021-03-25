cbea8ef2b8eb5b786ce7269643e75d516d923f32
Lee Warren <lee.warr@outlook.com>
2021-03-25 17:53:02 +1100

    Test commit...

--- Assets/Scripts/WorldSelectTile.cs
+++ Assets/Scripts/WorldSelectTile.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class WorldSelectTile : MonoBehaviour, IPointerClickHandler
 {
@@ -10,6 +11,8 @@ public class WorldSelectTile : MonoBehaviour, IPointerClickHandler
     RectTransform thisRectTransform;
 
     public Sprite plusImage;
+
+    public Text worldNumberText;
 
     public Vector2 position;
     public string worldName;
@@ -39,6 +42,7 @@ public class WorldSelectTile : MonoBehaviour, IPointerClickHandler
         position = new Vector2(index % 5, (index / 5));
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         worldName = thisWorldName;
+        worldNumberText.text = "" + (index + 1);
 
         PositionTileForWorldSelector();
     }
